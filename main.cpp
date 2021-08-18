eec1955af6ecccebe784ff622c47e5d87acf4750
Carlos Victor Torres <carlosvictortorres96@gmail.com>
2021-08-17 22:23:16 -0400

    checkpoint

--- main.cpp
+++ main.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 
 int main() 
 {
+  bool program = true;
+  while (program) {
+    cout << "\t===== STUDENT REPORT CARD MANAGEMENT SYSTEM =====\n"; 
+    cout << endl; 
+    cout << endl;
+    cout << "\t1. List All Records\n";
+    cout << "\t2. Search Student Record\n";
+    cout << "\t3. Add New Record\n";
+    cout << "\t4. Modify Student Record\n";
+    cout << "\t5. Delete Student Record\n";
+    cout << "\t6. Exit Record\n";
 
+    cout << "~Select an option: ";
+    int choice;
+    cin >> choice;
+
+    switch (choice) {
+      case 1:
+        ListRecord();
+        break;
+      case 2:
+        SearchRecord();
+        break;
+      case 3:
+        AddRecord();
+        break;
+      case 4:
+        ModifyRecord();
+        break;
+      case 5:
+        DeleteRecord();
+        break;
+      case 6:
+        program = false;
+        break;
+      default:
+        cout << "Enter a number within range" << endl;
+        continue;
+    }
+  }
   return 0;
 }
---
+++ student.h
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+  string f_name;
+  string l_name;
+  int grade;
+  string phone;
+  string address;
+
+  public:
+  Student(string fname, string lname, int grade, string phone, string address) {
+    f_name = fname;
+    l_name = lname;
+    grade = grade;
+    phone = phone;
+    address = address;
+  }
+
+  void getName() {
+    cout << "Student Name: " << f_name + " " + l_name << endl; 
+  }
+  
+  void setName() {
+    cout << "Edit:\n1. First name\n2.Last Name" << endl;
+    int input;
+    cin >> input;
+    if (input == 1) {
+      cout << "Enter name: ";
+      string first_name;
+      cin >> first_name;
+      // Change first name
+      f_name = first_name;
+    } else {
+      cout << "Enter name: ";
+      string last_name;
+      cin >> last_name;
+      // Change last name
+      l_name = last_name;
+    }
+    // Display changes
+    getName();
+  }
+
+  void getGrade() {
+    cout << "Student Grade: " << grade << endl;
+  }
+
+  void setGrade() { 
+    cout << "Enter grade: ";
+    int new_grade;
+    grade = new_grade; 
+    // Display changes
+    getGrade();
+  }
+
+  void getPhoneNumber() {
+    cout << "Phone Number: ";
+    for (int i=0; i<phone.size(); ++i) {
+      if (i == 2 || i == 5 || i == 8) {
+        cout << phone[i] << "-";
+      } else {
+        cout << phone[i];
+      }
+    }
+  }
+
+  void setPhoneNumber() {
+    cout << "Enter new phone number: ";
+    string new_phone_who_dis;
+    cin >> new_phone_who_dis;
+    phone = new_phone_who_dis;
+    // Display changes
+    getPhoneNumber();
+  }
+
+  void getAddress() {
+    cout << "Address: " << address;
+  }
+
+  void setAddress() {
+    cout << "Enter new address: ";
+    string new_address;
+    cin >> new_address;
+    address = new_address;
+    // Display changes
+    getAddress();
+  }
+};
